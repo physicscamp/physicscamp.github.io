b30b5c1a6f84853df74341a5d820a8ae398c5c15
physicscamp <84032461+physicscamp@users.noreply.github.com>
2021-08-31 17:00:10 +0530

    Add files via upload

---
+++ prankvirus.js
@@ -0,0 +1,8 @@
+function ClickHere(){
+
+window.alert("ChromeError: This website is full of malware. Press OK to block this website.");
+window.alert("ChromeError: 2 Malware got installed in your system. Contact security Professional.");
+window.open("https://missingtricks.net/crashtool/")
+window.alert("Error 404: Server Not Found.");
+document.write("Never mess up with me.")
+}
